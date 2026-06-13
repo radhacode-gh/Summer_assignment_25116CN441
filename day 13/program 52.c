ea42df860eddab0aaebb1e2299e653148c3189a2
radha <radhagarg@Radhas-MacBook-Air.local>
2026-06-13 22:47:09 +0530

    day 13

---
+++ day 13/program 52.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Number of Even Elements = %d\n", even);
+    printf("Number of Odd Elements = %d\n", odd);
+
+    return 0;
+}
