0f10f4c9e751adea5f3a4a2466d4816a42f4d47f
radha <radhagarg@Radhas-MacBook-Air.local>
2026-06-30 19:36:24 +0530

    day 23

---
+++ day 23/program 89.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, count;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        count = 0;
+
+        for(j = 0; str[j] != '\0'; j++) {
+            if(str[i] == str[j])
+                count++;
+        }
+
+        if(count == 1) {
+            printf("First Non-Repeating Character = %c", str[i]);
+            return 0;
+        }
+    }
+
+    printf("No Non-Repeating Character");
+
+    return 0;
+}
